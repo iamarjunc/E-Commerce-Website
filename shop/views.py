4e74ddb5bb37e41dfbefcaf92aa952ae98f07fc7
Arjun c <arjunchandrakumar2017@gmail.com>
2023-12-17 12:29:05 +0530

    commit 17 dec 2023

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import render
-from .models import Product, Contact, Orders
+from .models import OrderUpdate, Product, Contact, Orders
 from math import ceil
 # import the logging library
 import logging
@@ -44,6 +45,23 @@ def contact(request):
     return render(request, 'shop/contact.html')
 
 def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
 
 def search(request):
@@ -58,18 +76,20 @@ def productView(request, myid):
 
 def checkout(request):
     if request.method=="POST":
-        items_json= request.POST.get('itemsJson', '')
-        name=request.POST.get('name', '')
-        email=request.POST.get('email', '')
-        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
-        city=request.POST.get('city', '')
-        state=request.POST.get('state', '')
-        zip_code=request.POST.get('zip_code', '')
-        phone=request.POST.get('phone', '')
-
-        order = Orders(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
         order.save()
-        thank=True
-        id=order.order_id
-        return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
     return render(request, 'shop/checkout.html')
